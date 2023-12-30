f43e094cd5dd8d6d553dcb9c676a41ab50c74024
Rutracrafter <artur.melo.rodrigues@gmail.com>
2023-12-29 19:36:15 -0800

    finished the base implementation of the backend, now going to work on the front end

--- backend/dogtrainingbackend/dogtraining/urls.py
+++ backend/dogtrainingbackend/dogtraining/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import DogsView, UsersView
+from .views import DogsView, UsersView, DogsByOwner, OwnersByDog
 
 urlpatterns = [
     path('dogs/', DogsView.as_view()),
     path('users/', UsersView.as_view()),
+    path('dogs_by_owner/', DogsByOwner),
+    path('owners_by_dog/<int:dog_id>', OwnersByDog),
 ]
--- backend/dogtrainingbackend/dogtraining/views.py
+++ backend/dogtrainingbackend/dogtraining/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from rest_framework import generics
 from .models import Dog
 from django.contrib.auth.models import User
@@ -14,3 +15,15 @@ class DogsView(generics.ListAPIView):
 class UsersView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+def DogsByOwner(request):
+    user_id = request.user.id
+    dogs = Dog.objects.filter(owners=user_id)
+    data = serializers.serialize('json', dogs)
+    return HttpResponse(data)
+
+def OwnersByDog(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    owners = dog.owners.all()
+    data = serializers.serialize('json', owners, fields=["username"])
+    return HttpResponse(data)
