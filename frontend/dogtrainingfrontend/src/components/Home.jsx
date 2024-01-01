c268bcb35d329f06f6b4b2f6c159f55bde3e67cf
Rutracrafter <artur.melo.rodrigues@gmail.com>
2023-12-31 17:23:52 -0800

    started using Material Tailwind component library and made dogs table with the Table component, thinking about switching to PrimeReact

--- frontend/dogtrainingfrontend/src/components/Home.jsx
+++ frontend/dogtrainingfrontend/src/components/Home.jsx
@@ -2,28 +2,231 @@ import React, { useState, useEffect } from 'react'
 import DogCard from "./DogCard";
 import axios from 'axios'
 
+import {
+  MagnifyingGlassIcon,
+  ChevronUpDownIcon,
+} from "@heroicons/react/24/outline";
+import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
+import {
+  Card,
+  CardHeader,
+  Input,
+  Typography,
+  Button,
+  CardBody,
+  Chip,
+  CardFooter,
+  Tabs,
+  TabsHeader,
+  Tab,
+  Avatar,
+  IconButton,
+  Tooltip,
+} from "@material-tailwind/react";
+
 const Home = () => {
   const [dogsData, setDogsData] = useState([]);
+  let TABLE_ROWS;
 
   useEffect(() => {
     const Url = 'http://127.0.0.1:8000/dogs/';
     axios.get(Url)
     .then((response) => {
       setDogsData(response.data)
-      console.log(dogsData)
+      console.log(TABLE_ROWS)
     })
     .catch(() => {
       console.log("Error fetching data from {Url}")
     })
   }, []);
 
+  TABLE_ROWS = JSON.parse(JSON.stringify(dogsData));
+  for (let item of TABLE_ROWS) {
+    for (let key of Object.keys(item)) {
+      if (key == "progress") {
+        let total = Object.keys(item[key]).length;
+        let progress = 0;
+        for (let milestone of Object.keys(item[key])) {
+          if (item[key][milestone]) {
+            progress++;
+          }
+        }
+        item[key] = `${progress} / ${total}`
+      }
+      if (key == "creation_date") {
+        item[key] = ((item[key]).split('T'))[0]
+      }
+    }
+  }
+
+  const TABS = [
+    {
+      label: "All",
+      value: "all",
+    },
+    {
+      label: "Monitored",
+      value: "monitored",
+    },
+    {
+      label: "Unmonitored",
+      value: "unmonitored",
+    },
+  ];
+
+  const TABLE_HEAD = ["Dog", "Food", "Progress", "Creation Date", ""];
+
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-        <div className='bg-blue-gray-200 w-[60%] h-[70%] rounded-xl flex justify-center items-center'>
-            <div className='bg-red-200 w-[70%] h-[80%] rounded-xl overflow-y-auto'>
-                {dogsData.map((dog, index) => <DogCard key={index} photo={dog.photo} name={dog.name} age={dog.age} />)}
+      <Card className="w-[60%] h-[70%]">
+        <CardHeader floated={false} shadow={false} className="rounded-none">
+          <div className="mb-8 flex items-center justify-between gap-8">
+            <div>
+              <Typography variant="h5" color="blue-gray">
+                Dogs List
+              </Typography>
+              <Typography color="gray" className="mt-1 font-normal">
+                See information about all dogs
+              </Typography>
             </div>
-        </div>
+            <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+              <Button variant="outlined" size="sm">
+                view all
+              </Button>
+              <Button className="flex items-center gap-3" size="sm">
+                <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add member
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+            <Tabs value="all" className="w-full md:w-max">
+              <TabsHeader>
+                {TABS.map(({ label, value }) => (
+                  <Tab key={value} value={value}>
+                    &nbsp;&nbsp;{label}&nbsp;&nbsp;
+                  </Tab>
+                ))}
+              </TabsHeader>
+            </Tabs>
+            <div className="w-full md:w-72">
+              <Input
+                label="Search"
+                icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+              />
+            </div>
+          </div>
+        </CardHeader>
+        <CardBody className="overflow-scroll px-0">
+          <table className="mt-4 w-full min-w-max table-auto text-left">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head, index) => (
+                  <th
+                    key={head}
+                    className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                    >
+                      {head}{" "}
+                      {index !== TABLE_HEAD.length - 1 && (
+                        <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                      )}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {TABLE_ROWS.map(
+                ({ photo, name, age, food, progress, creation_date }, index) => {
+                  const isLast = index === TABLE_ROWS.length - 1;
+                  const classes = isLast
+                    ? "p-4"
+                    : "p-4 border-b border-blue-gray-50";
+
+                  return (
+                    <tr key={name}>
+                      <td className={classes}>
+                        <div className="flex items-center gap-3">
+                          <Avatar src={photo} alt={name} size="sm" />
+                          <div className="flex flex-col">
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-normal"
+                            >
+                              {name}
+                            </Typography>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-normal opacity-70"
+                            >
+                              {age} years old
+                            </Typography>
+                          </div>
+                        </div>
+                      </td>
+                      <td className={classes}>
+                        <div className="flex flex-col">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {food}%
+                          </Typography>
+                        </div>
+                      </td>
+                      <td className={classes}>
+                        <div className="w-max">
+                          <Chip
+                            variant="ghost"
+                            size="sm"
+                            value={progress}
+                          />
+                        </div>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {creation_date}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Tooltip content="Edit User">
+                          <IconButton variant="text">
+                            <PencilIcon className="h-4 w-4" />
+                          </IconButton>
+                        </Tooltip>
+                      </td>
+                    </tr>
+                  );
+                },
+              )}
+            </tbody>
+          </table>
+        </CardBody>
+        {/* <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+          <Typography variant="small" color="blue-gray" className="font-normal">
+            Page 1 of 10
+          </Typography>
+          <div className="flex gap-2">
+            <Button variant="outlined" size="sm">
+              Previous
+            </Button>
+            <Button variant="outlined" size="sm">
+              Next
+            </Button>
+          </div>
+        </CardFooter> */}
+      </Card>
     </div>
   )
 }
